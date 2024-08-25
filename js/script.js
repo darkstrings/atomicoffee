eb568c13d82ad1e405c82a3afdea7bb841365b77
Lucien Gaydos <darkstrings@gmx.com>
2024-08-25 19:42:26 -0400

    slight tweak

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-// Set current year
 const headerEle = document.querySelector(".header");
 const heroSectionEl = document.querySelector(".section-hero");
 const mobileNav = document.querySelector(".header");
@@ -8,6 +7,7 @@ function toggleNav() {
   mobileNav.classList.toggle("nav-open");
 }
 
+// Set current year
 document.querySelector(".year").innerText = new Date().getFullYear();
 
 const obs = new IntersectionObserver(
@@ -15,15 +15,11 @@ const obs = new IntersectionObserver(
     const ent = entries[0];
     if (!ent.isIntersecting) {
       document.body.classList.add("sticky");
-      console.log(ent);
     } else {
       document.body.classList.remove("sticky");
     }
-    // const addRemove = !ent.isIntersecting ? "add" : "remove";
-    // headerEl.classList[addRemove]("sticky");
   },
   {
-    // inside the viewport
     root: null,
 
     // As soon as 0% of the hero section is in the viewport....when it's 100% out....0% of it is in (hense the zero)
